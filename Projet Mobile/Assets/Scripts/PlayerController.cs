8dd8de336167d80dd1af7dca4b2e6ddb8a0b0216
Unknown <matthieu.manuellan@gmail.com>
2019-09-12 16:48:16 +0200

    jesaispas

--- Projet Mobile/Assets/Scripts/PlayerController.cs
+++ Projet Mobile/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 1.0f;
+    [SerializeField]
+    private float _rotateAmount = 1.0f;
 
     [SerializeField]
     private float _jumpHeight = 5.0f;
 
     private Rigidbody _rb;
+
 
     private void Awake()
     {
@@ -19,7 +22,8 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(new Vector3(0, 0, _speed * Time.deltaTime));
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + _rotateAmount * Time.deltaTime, transform.rotation.eulerAngles.z);
+        _rb.velocity = transform.TransformDirection(new Vector3(0, 0, _speed));
     }
     public void Jump()
     {
