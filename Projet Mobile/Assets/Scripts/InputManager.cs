09c125c91d84c8b3d3e08e4bbba0b0010aafd71b
Matt <matthieu.manuellan@gmail.com>
2019-09-14 19:12:17 +0200

    switch Path

--- Projet Mobile/Assets/Scripts/InputManager.cs
+++ Projet Mobile/Assets/Scripts/InputManager.cs
@@ -9,7 +9,7 @@ public class InputManager : MonoBehaviour
 
     
     [SerializeField]
-    private PlayerController _player;
+    private MoveOnPath _player;
 
     private float _endRadius;
     public void InputPlayer(SwipeData data)
@@ -17,16 +17,38 @@ public class InputManager : MonoBehaviour
         switch(data.Direction)
         {
             case SwipeDirection.Left:
-                if (_player._radius > _player._minRadius)
-                    _player.MoveLeft();
+                if (_player._pathId > 0)
+                {
+                    _player._pathId--;
+                    _player.transform.position = _player.transform.position + transform.TransformDirection(Vector3.left);
+                }
                 break;
             case SwipeDirection.Right:
-                if (_player._radius < _player._maxRadius)
-                    _player.MoveRight();
+                if (_player._pathId < 2)
+                {
+                    _player.transform.position = _player.transform.position + transform.TransformDirection(Vector3.right);
+                    _player._pathId++;
+                }
                 break;
             case SwipeDirection.Up:
-                _player.Jump();
+                //_player.Jump();
                 break;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            SwipeData _data = new SwipeData();
+            _data.Direction = SwipeDirection.Left;
+            InputPlayer(_data);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SwipeData _data = new SwipeData();
+            _data.Direction = SwipeDirection.Right;
+            InputPlayer(_data);
         }
     }
 }
