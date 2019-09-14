555d05b58f0525cfce8a0dd06c93ffe91df44f26
Matt <matthieu.manuellan@gmail.com>
2019-09-14 18:36:09 +0200

    Path

---
+++ Projet Mobile/Assets/MoveOnPath.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnPath : MonoBehaviour
+{
+    public PathEditor _pathToFollow;
+    public int _currentWayPointId = 0;
+    public float _moveSpeed;
+    private float _reachDistance = 0.5f;
+    public float _rotationSpeed = 0.5f;
+    public string _pathName;
+
+    Vector3 _lastPosition;
+    Vector3 _currentPosition;
+
+    private void Start()
+    {
+        //_pathToFollow = GameObject.Find(_pathName).GetComponent<PathEditor>();
+        _lastPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(_pathToFollow._wayPoints[_currentWayPointId].position, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, _pathToFollow._wayPoints[_currentWayPointId].position, Time.deltaTime * _moveSpeed);
+
+        if(distance <= _reachDistance)
+        {
+            _currentWayPointId++;
+        }
+
+        if (_currentWayPointId >= _pathToFollow._wayPoints.Count)
+            _currentWayPointId = 0;
+    }
+
+}
---
+++ Projet Mobile/Assets/PathEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathEditor : MonoBehaviour
+{
+    public Color _rayColor = Color.white;
+    public List<Transform> _wayPoints = new List<Transform>();
+    Transform[] _array;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _rayColor;
+        _array = GetComponentsInChildren<Transform>();
+        _wayPoints.Clear();
+        foreach(Transform wayPoint in _array)
+        {
+            if(wayPoint != this.transform)
+            {
+                _wayPoints.Add(wayPoint);
+            }
+        }
+
+        for(int i = 0; i < _wayPoints.Count; i++)
+        {
+            Vector3 position = _wayPoints[i].position;
+            if(i > 0)
+            {
+                Vector3 previous = _wayPoints[i - 1].position;
+                Gizmos.DrawLine(previous, position);
+                Gizmos.DrawWireSphere(position,0.3f);
+            }
+        }
+    }
+}
